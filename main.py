ae9e7b12f12332825c9fc5942ddd275e6e2b6e05
anonim226757 <adrozdov0777@gmail.com>
2025-02-28 17:53:01 +0000

    продолжаем работать

--- main.py
+++ main.py
@@ -31,3 +31,46 @@ def load_image(
     colorkey=None'
 ):
 
+    fullname = os.path.join('sprites', name)
+    image = pygame.image.load(fullname)
+    image = image.convert()
+    if colorkey is -1:
+        colorkey = image.get_at((0, 0))
+    image.set_colorkey(colorkey, RLEACCEL)
+
+  is sizex != -1 sizey != -1:
+    image = pygame.transfrom.scale(image, (sizex, sizey))
+
+    return (image, image.get_rect())
+ 
+def  load_sprite_rect(
+        sheetname,
+        nx,
+        ny,
+        skalex = -1,
+        skaley = -1,
+        colorkey = None
+    ):
+    fullname = os.path.join('sprites', sheetname)
+    sheet = pygame.image
+    sheet = sheet.convert()
+
+    sheet_rect = sheet.get_rect()
+
+    sprites = []
+
+    sizex = sheet_rect.width/nx
+    sizey = sheet_rect.height/ny
+
+    for i range(0,ny)
+        for j  in range(0, nx)
+            rect = pygame.Rect((j*sizey, sizex, sizey))
+            image =  pygame.Surface(rect.size)
+            image = image.convert()
+            image.blit(sheet,(0,0), rect)
+
+            if colorkey is not None:
+                if colorkey = image.get_at((0,0))
+                image.set_colorkey(colorkey, RLEACCEL)
+
+            if scalex != -1 or scaley != -1

a32c532ecc2d688587037d7a99afa256ba2c6ae1
anonim226757 <adrozdov0777@gmail.com>
2025-03-21 17:53:55 +0000

    ждем

--- main.py
+++ main.py
@@ -100,5 +100,75 @@ def  load_sprite_rect(
             digits.append(number%10)
             number = int(number/10)
 
-        digits.append(number%10)
-        
+        digits.append(number%10) 
+        def draw(self):
+        screen.blit(self.image,self.rect)
+
+    def checkbounds(self):
+        if self.rect.bottom > int(0.98*height):
+            self.rect.bottom = int(0.98*height)
+            self.isJumping = False
+
+    def update(self):
+        if self.isJumping:
+            self.movement[1] = self.movement[1] + gravity
+
+        if self.isJumping:
+            self.index = 0
+        elif self.isBlinking:
+            if self.index == 0:
+                if self.counter % 400 == 399:
+                    self.index = (self.index + 1)%2
+            else:
+                if self.counter % 20 == 19:
+                    self.index = (self.index + 1)%2
+
+        elif self.isDucking:
+            if self.counter % 5 == 0:
+                self.index = (self.index + 1)%2
+        else:
+            if self.counter % 5 == 0:
+                self.index = (self.index + 1)%2 + 2
+
+        if self.isDead:
+           self.index = 4
+
+        if not self.isDucking:
+            self.image = self.images[self.index]
+            self.rect.width = self.stand_pos_width
+        else:
+            self.image = self.images1[(self.index)%2]
+            self.rect.width = self.duck_pos_width
+
+        self.rect = self.rect.move(self.movement)
+        self.checkbounds()
+
+        if not self.isDead and self.counter % 7 == 6 and self.isBlinking == False:
+            self.score += 1
+            if self.score % 100 == 0 and self.score != 0:
+                if pygame.mixer.get_init() != None:
+                    checkPoint_sound.play()
+
+        self.counter = (self.counter + 1)
+ class Cactus(pygame.sprite.Sprite):
+            def __init__(self,speed=5,sizex=1,sizey=1):   
+                pygame.sprite.Sprite.__init__(selft,self.containers)
+                self.images,self.rect =loand_sprite_sheet('cacti-small.png',3,1,sizex,sizey,-1)
+                self.rect.bottom = int(0.95*height)
+                self.rect.left = width + self.rect.width
+                self.image = self.images[random.randrange(0,3)]
+                self.movement = [-1*speed,0]
+
+            def draw(self):
+                screen.blit(self.image,self.rect)
+
+            def update(self):
+                self.rect = self.rect.move(self.movement)
+
+                if self.rect.right < 0:
+                    self.kill()
+
+        class Ptera(pygame.sprite.Sprite):
+            def __init__(self,speed=5,sizex=1,sizey=1):
+                pygame.sprite.Sprite.__init__(self,self.containers)
+                self.images,
